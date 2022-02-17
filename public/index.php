d2a573ee5b6b8ad18211a923e9ec6042600261e7
pgh97 <pgh97@com2us.com>
2022-02-17 20:01:16 +0900

    API 설계 및 slim 학습

---
+++ public/index.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\config\db\Database;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ ."/../src/config/db_connection.php";
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->add(new BasePathMiddleware($app));
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('public Hello World!');
+    return $response;
+});
+
+$app->get('/fish-info-data/all', function (Request $request, Response $response) {
+    $sql = "SELECT * FROM fish_info_data";
+
+    try {
+        $db = new Database();
+        $conn = $db->getConnection();
+        $stmt = $conn->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+
+        $response->getBody()->write(json_encode($customers));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch (PDOException $e) {
+        $error = array(
+            "message" => $e->getMessage()
+        );
+
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+$app->run();
